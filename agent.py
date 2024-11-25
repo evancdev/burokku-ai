9237ef8f6c49b94641f85b83603ebec6368d5f96
Nelson <nelsonfeng.18@gmail.com>
2024-11-25 00:18:57 -0500

    Changed inputs and reformmated DQNAgent (WIP)
    - Epsillon decayed is calculated using epsilon, espilon_min, and epsilon_stop_episode

--- agent.py
+++ agent.py
@@ -1,6 +1,6 @@
 from keras import Model
 from keras import Sequential
-from tensorflow.python.keras.layers import Dense
+from keras.layers import Dense
 from collections import deque
 import numpy as np
 import random
@@ -14,20 +14,21 @@ class DQNAgent:
     - state_size (int): The size of the input state from the environment
     - buffer_size (int): Size of replay buffer
     - batch_size (int): Size of sampled batch from replay buffer
-    - discount(float): Discount factor
+    - gamma (float): Discount factor
     - epsilon (float): Exploration rate
     - epsilon_min (float): Minimum exploration rate
-    - epsilon_stop_episode (int) : The episode the agent stops decreasing epsilon
     - epsilon_decay (float): Exploration decay rate
+    - epsilon_stop_episode (int): What episode does agent stop decr. epsilon
+
     - n_neurons (list(int)): list of number of neurons in each inner layer
     - activations (list): list of activations used in each inner layer
     - loss_fun (obj): loss function object
     - optimizer (obj): optimizer object
     """
 
-    def __init__(self, state_size,
-                 buffer_size, batch_size, discount, epsilon,
-                 epsilon_min, epsilon_stop_episode, n_neurons, activations, loss_fun, optimizer):
+    def __init__(self, state_size, buffer_size, batch_size, 
+                discount, epsilon, epsilon_min, epsilon_stop_episode,
+                n_neurons, activations, loss_fun, optimizer):
 
         self.discount = discount
         self.state_size = state_size
@@ -36,8 +37,7 @@ class DQNAgent:
         self.mem = deque(maxlen=buffer_size)
         self.epsilon = epsilon
         self.epsilon_min = epsilon_min
-        self.epsilon_decay = (
-            self.epsilon - self.epsilon_min) / epsilon_stop_episode
+        self.epsilon_decay = (self.epsilon - self.epsilon_min) / epsilon_stop_episode
         self.n_neurons = n_neurons
         self.activations = activations
         self.loss_fun = loss_fun
@@ -46,26 +46,32 @@ class DQNAgent:
         self.model = self.build_model()
 
     def build_model(self):
-        """
-        Builds a Keras dense neural network
-        """
+        """Builds a Keras deep neural network model"""
 
         model = Sequential()
-        model.add(Dense(
-            self.n_neurons[0], input_dim=self.state_size, activation=self.activations[0]))
+        model.add(Dense(self.n_neurons[0], input_dim=self.state_size, activation=self.activations[0]))
+
         for i in range(1, len(self.n_neurons)):
             model.add(Dense(self.n_neurons[i], activation=self.activations[i]))
 
         model.add(Dense(1, activation=self.activations[-1]))
+
         model.compile(loss=self.loss_fun, optimizer=self.optimizer)
 
         return model
 
+    
     def predict_output(self, state):
-        """
-        Predicts score output from a given state
-        """
-        return self.model.predict(state)[0]
+       """
+       Predicts score output from a given state
+        
+        Parameters:
+        - state (np.array): The current state of the environment
+
+        Returns:
+        - score (int): The expected score from a certain state
+       """
+       return self.model.predict(state)[0]
 
     def act(self, state):
         """
@@ -74,22 +80,21 @@ class DQNAgent:
         Parameters:
         - state (np.array): The current state of the environment
 
-      Returns:
-      - score (int): The expected score from a certain state
-      """
+        Returns:
+        - score (int): The expected score from a certain state
+        """
 
         if random.random() <= self.epsilon:
             return self.random_qvalue()
         else:
-            # First dim represents batch size of one
-            state = np.reshape(state, [1, self.state_size])
+            state = np.reshape(state, [1, self.state_size]) # First dim represents batch size of one
             return self.predict_output(state)
-
+      
     def random_output(self):
-        """
-        Returns a random score output
-        """
-        return random.random()
+       """
+       Returns a random score output
+       """
+       return random.random()
 
     def remember(self, state, action, reward, next_state, done):
         """
@@ -104,42 +109,41 @@ class DQNAgent:
         """
         self.mem.append((state, next_state, reward, done))
 
-    def train(self, epochs=5):
+    def train(self, epochs = 5):
         """
         Samples batch of experiences and train them
         """
         n = len(self.mem)
 
-        ### CHECK IF CONDITION GOOD ###
+        ### CHECK IF CONDITION GOOD ### 
         if n >= self.batch_size and n >= self.buffer_size:
             batch = random.sample(self.mem, self.batch_size)
-
+            
             next_states = np.array((x[1] for x in batch))
 
             # Generate Q-values for every possible next states
             next_q_values = []
             for x in self.model.predict(next_states):
                 next_q_values.append(x[0])
-
+            
             X = []
             Y = []
 
             # batch has parameters (current_state, action, next_state, reward, done)
             for i, (current_state, action, next_state, reward, done) in enumerate(batch):
                 if done:
-                    new_q_value = reward
+                    new_q_value= reward
                 else:
                     # reward for taking in a state + discount rate * (max reward from future)
-                    new_q_value = reward + self.discount * \
-                        np.max(next_q_values[i])
-
+                    new_q_value = reward + self.discount * np.max(next_q_values[i])
+            
                 X.append(current_state)
                 Y.append(new_q_value)
-
+              
             # Fit model
-            self.model.fit(np.array(x), np.array(
-                y), batch_size=self.batch_size, epochs=epochs)
+            self.model.fit(np.array(x), np.array(y), batch_size = self.batch_size, epochs = epochs)
 
             # Update epsilon
             if self.epsilon > self.epsilon_min:
                 self.epsilon -= self.epsilon_decay
+                
