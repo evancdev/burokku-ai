480714804be66df7fee1e499f337b703e532e8a3
Evan Chen <cs.evanc@gmail.com>
2024-11-25 01:26:34 -0500

    Update run-agent.py
    - Edit while condition
    - Added print statements to debug

--- run-agent.py
+++ run-agent.py
@@ -4,71 +4,64 @@ from heuristic import TetrisAI
 from tqdm import tqdm
 
 
-class AgentParam():
+class AgentParam:
     def __init__(self):
         self.discount = 0.9
         self.state_size = 4
         self.batch_size = 512
         self.buffer_size = 1000
         self.epsilon = 1.0
-        self.epsilon_min = 0.0
+        self.epsilon_min = 0.01
         self.n_neurons = [32, 32]
         self.activations = ['relu', 'relu', 'linear']
         self.loss_fun = 'mse'
         self.optimizer = 'adam'
         self.epsilon_stop_episode = 2000
         self.max_steps = 1000
-        self.episodes = 2000  
+        self.episodes = 2000
 
 
-
-def run_dqn(agentparam : AgentParam):
-    # Run dqn with Tetris game
+def run_dqn(agentparam: AgentParam):
     tetris = Tetris()
     scores = []
 
     agent = DQNAgent(
-    discount = agentparam.discount,
-    state_size = agentparam.state_size,
-    batch_size = agentparam.batch_size,
-    buffer_size = agentparam.buffer_size,
-    epsilon = agentparam.epsilon,
-    epsilon_min = agentparam.epsilon_min,
-    n_neurons = agentparam.n_neurons,
-    activations = agentparam.activations,
-    loss_fun = agentparam.loss_fun,
-    optimizer = agentparam.optimizer,
-    epsilon_stop_episode = agentparam.epsilon_stop_episode
-    )   
+        discount=agentparam.discount,
+        state_size=agentparam.state_size,
+        batch_size=agentparam.batch_size,
+        buffer_size=agentparam.buffer_size,
+        epsilon=agentparam.epsilon,
+        epsilon_min=agentparam.epsilon_min,
+        n_neurons=agentparam.n_neurons,
+        activations=agentparam.activations,
+        loss_fun=agentparam.loss_fun,
+        optimizer=agentparam.optimizer,
+        epsilon_stop_episode=agentparam.epsilon_stop_episode
+    )
 
-    for episodes in tqdm(range(agentparam.episodes)):
+    for episode in tqdm(range(agentparam.episodes)):
         curr_state = tetris.reset()
         done = False
         step = 0
 
-        while not done and (not agentparam.max_steps or step < agentparam.max_steps):
+        while not done and (agentparam.max_steps == 0 or step < agentparam.max_steps):
             next_states = tetris.get_next_states()
-            best_state = agent.get_best_state(next_states.values())
+            print(next_states)
+            best_state = agent.get_best_state(list(next_states.values()))
+            print(best_state)
 
+            # Find the action that leads to the best state
             best_action = None
             for action, state in next_states.items():
                 if state == best_state:
                     best_action = action
-                    break
-
-
-            # print(best_action)
-            # piece = tetris.rotate_piece(best_action[1])
             reward, done = tetris.play(best_action[0])
             agent.remember(curr_state, next_states[best_action], reward, done)
             curr_state = next_states[best_action]
             step += 1
             agent.train()
 
-        scores.append(tetris.get_score())
-
 
 if __name__ == "__main__":
-    agentparam = AgentParam()   
-
+    agentparam = AgentParam()
     run_dqn(agentparam)
